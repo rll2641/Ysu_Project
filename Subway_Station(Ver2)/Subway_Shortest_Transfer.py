9e92d7c0c8cbb9b7a965428f6b1146547fdc1044
rll2641 <fair0838@gmail.com>
2022-10-23 19:59:01 +0900

    지하철 최단경로

--- Subway_Station(Ver2)/Subway_Shortest_Transfer.py
+++ Subway_Station(Ver2)/Subway_Shortest_Transfer.py
@@ -2,34 +2,7 @@
 from collections import deque
 from collections import defaultdict
 import Subway_Information as si
-
-def transfer(route):
-    
-    if len(route) < 2:
-        return 0
-    
-    # 출발점의 호선을 기억하는 함수
-    def select_start_line(route, idx):
-        start_line = si.station_information[route[idx]]['호선']
-        
-        # 출발점의 호선이 2개 이상일 경우
-        if len(start_line) > 1:
-            start_line = "".join([i for i in start_line if i in si.station_information[route[idx + 1]]['호선']])
-        else:
-            start_line = "".join(start_line)
-        
-        return start_line
-    
-    start_line = select_start_line(route, 0)
-    
-    # 환승 발생
-    transfer_cnt = 0
-    for idx, station in enumerate(route):
-        if start_line not in si.station_information[station]['호선']:
-            start_line = select_start_line(route, idx-1)
-            transfer_cnt += 1
-    
-    return transfer_cnt
+import Subway_Transfer as st
 
 def shortest_transfer(start, end, i = 0):
     transfer_route = defaultdict(deque) 
@@ -55,7 +28,7 @@ def shortest_transfer(start, end, i = 0):
                 visited.append(j)
                 new_route = list(route)
                 new_route.append(j)
-                i = transfer(new_route)
+                i = st.transfer_dict(new_route)
                 transfer_route[i].append(new_route)
                 
                 if new_route[-1] == end and i < 2:
--- Subway_Station(Ver2)/Subway_Transfer.py
+++ Subway_Station(Ver2)/Subway_Transfer.py
@@ -36,3 +36,31 @@ def transfer(route):
             tr_str.append(save.pop(0) + '역 ' + '->'.join(save))
     
     return tr_str
+
+def transfer_dict(route):
+    
+    if len(route) < 2:
+        return 0
+    
+    # 출발점의 호선을 기억하는 함수
+    def select_start_line(route, idx):
+        start_line = si.station_information[route[idx]]['호선']
+        
+        # 출발점의 호선이 2개 이상일 경우
+        if len(start_line) > 1:
+            start_line = "".join([i for i in start_line if i in si.station_information[route[idx + 1]]['호선']])
+        else:
+            start_line = "".join(start_line)
+        
+        return start_line
+    
+    start_line = select_start_line(route, 0)
+    
+    # 환승 발생
+    transfer_cnt = 0
+    for idx, station in enumerate(route):
+        if start_line not in si.station_information[station]['호선']:
+            start_line = select_start_line(route, idx-1)
+            transfer_cnt += 1
+    
+    return transfer_cnt
